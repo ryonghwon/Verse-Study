74ff618824fbbc1c0f4549d7d47fd90fcbb8f652
vp-yongwon <yongwonkim@viralpick.co.kr>
2023-03-21 10:50:50 +0900

    [feat] 2023.03.20 진행

--- publishing/practice/09_image-frame-control-with-scroll-event/assets/js/app.js
+++ publishing/practice/09_image-frame-control-with-scroll-event/assets/js/app.js
@@ -8,6 +8,11 @@ const APP = {
     _imageName: 'airpods-max_frame-',
     _extension: 'jpg',
     _frames: [],
+
+    _rotateDistance: 700,
+    _teleDistance: 300,
+    _teleAmount: 100,
+
     _max: 45,
     _width: 1004,
     _height: 1214,
@@ -33,9 +38,14 @@ const APP = {
         this.frameEl = this.productVisualEl.querySelector('#frame');
     },
     addEvent() {
+        window.addEventListener('resize', this.handleResizeWindow.bind(this));
         window.addEventListener('scroll', this.handleScrollWindow.bind(this), { passive: true });
     },
     reset() {
+        this._rotateProgressRatio = this._rotateDistance / (this._rotateDistance +  this._teleDistance);
+        this._canvasRatio = this._width / this._height;
+        this._startScale = 1 / 1.5;
+        window.dispatchEvent(new Event('resize'));
         window.dispatchEvent(new Event('scroll'));
     },
     drawCanvas() {
@@ -62,7 +72,7 @@ const APP = {
             image.src = imageUrl;
         }
     },
-    drawFrame() {
+    drawFrame(ratio = 0) {
         this.context.clearRect(0, 0, this._width, this._height);
         // const image = new Image();
         // image.onload = () => {
@@ -71,7 +81,8 @@ const APP = {
         // }
         // image.src = '/assets/img/airpods-max_frame-0000.jpg';
         // drawImage - 이미지 로드가 완료된 후에 적용할 수 있다.
-        this.context.drawImage(this._frames[0], 0, 0, this._width, this._height)
+        const id = Math.round(Math.lerp(0, this._max - 1, ratio));
+        this.context.drawImage(this._frames[id], 0, 0, this._width, this._height);
         // this.context.fillStyle = "blue";
         // const rectWidth = 300;
         // const rectHeight = 500;
@@ -92,8 +103,45 @@ const APP = {
         const { y, height } = this.stickyWrapEl.getBoundingClientRect();
         const limit = height - window.innerHeight;
         if (y <= 0 && limit + y >= 0) {
-            // console.log(y, limit);
+            const current = Math.min(1, (y * -1) / this._rotateDistance);
+            const scaleRatio = (y * -1) / limit;
+            const visualScale = this._startScale + Math.lerp(0, 1 - this._startScale, scaleRatio);
+            gsap.set(this.productVisualEl, { scale: visualScale });
+            if (current > this._rotateProgressRatio) {
+                this.canvasEl.classList.add('hidden');
+                this.cupsEl.classList.add('visible');
+                this.frameEl.classList.add('visible');
+            } else {
+                this.canvasEl.classList.remove('hidden');
+                this.cupsEl.classList.remove('visible');
+                this.frameEl.classList.remove('visible');
+                gsap.set(this.frameEl, { y: 0 });
+            }
+            if (current <= this._rotateProgressRatio) {
+                const ratio = Math.lerp(0, 1, current / this._rotateProgressRatio);
+                return this.drawFrame(ratio);
+            }
+            // const frameY = Math(0, 1, current / this._r)
+            // --- rotate 애니메이션이 완료된 이후의 스크롤 시작 부터 ~
+            // 700 ~ 1000
+            const frameY = (current - this._rotateProgressRatio) / (1 - this._rotateProgressRatio) * this._teleAmount * -1;
+            gsap.set(this.frameEl, { y: frameY });
+            return this.context.clearRect(0, 0, this._width, this._height);
         }
+        if (y > 0) {
+            return gsap.set(this.productVisualEl, { scale: this._startScale });
+        }
+        gsap.set(this.productVisualEl, { scale: 1.0 });
+    },
+    handleResizeWindow() {
+        const { innerWidth: windowWidth, innerHeight: windowHeight } = window
+        this._visualRatio = this.productVisualEl.offsetWidth / this.productVisualEl.offsetHeight;
+        this._stickyInnerHeight =
+            this._visualRatio > this._canvasRatio ?
+                windowHeight * 1.5 :
+                windowWidth * this._canvasRatio * 1.5;
+        this.stickyInnerEl.style.height = `${this._stickyInnerHeight}px`;
+        this.stickyWrapEl.style.height = `${this._stickyInnerHeight + this._rotateDistance + this._teleDistance}px`;
     },
     handleScrollWindow() {
         if (this._isEnabled) {
